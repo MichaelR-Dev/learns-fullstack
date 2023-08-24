373a61e803e2a3cc34eaac6f4de2343dfb285303
MichaelR-Dev <kmedrang@gmail.com>
2023-08-24 03:18:07 -0400

    Update middleware and loginform, add route

--- webrtc-chat/src/components/LoginForm.tsx
+++ webrtc-chat/src/components/LoginForm.tsx
@@ -6,6 +6,153 @@ const Validator = require('validator');
 
 export default function LoginForm(){
 
+    async function loginUser(event: React.FormEvent<HTMLFormElement>): Promise<void>{
+        event.preventDefault();
+    
+        const URL: RequestInfo = "http://127.0.0.1:8090/api/collections/users/auth-with-password";
+        const FORMDATA: FormData = new FormData(event.currentTarget);
+        const headers: Headers = new Headers();
+    
+        headers.set('Content-Type', 'application/json')
+    
+        //headers.append('Authorization', `Bearer ${}`)
+    
+        let JSONRequest: any = {};
+        FORMDATA.forEach((value, key) => {
+    
+            if(key === 'email'){
+                value = Validator.normalizeEmail(value);
+            }
+    
+            JSONRequest[key] = Validator.escape(value);
+        
+        });
+    
+        //POST request to auth
+        await fetch(URL, {
+    
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(JSONRequest),
+    
+        })
+        .then(response => response.json())
+        .then(data => document.cookie = `${encodeURIComponent('njsa')}=${encodeURIComponent('Bearer')} ${encodeURIComponent(data.token)}`)
+        .catch(error => {
+            
+            alert(`Error ${error.code}\n${error.message}`)
+            console.log(error)
+        
+        })
+    
+    }
+    
+    async function registerUser(event: React.FormEvent<HTMLFormElement>): Promise<void>{
+        event.preventDefault();
+    
+        const URL: RequestInfo = "http://127.0.0.1:8090/api/collections/users/records";
+        const FORMDATA: FormData = new FormData(event.currentTarget);
+        const headers: Headers = new Headers();
+    
+        headers.append('Content-Type', 'application/json');
+    
+    
+        if(!Validator.isEmail(FORMDATA.get('email'))){
+    
+            
+            alert('Invalid Email');
+    
+            return;
+    
+        }
+    
+        if(Validator.isStrongPassword(FORMDATA.get('password'), 
+        { 
+            minLength: 8, 
+            minLowercase: 1, 
+            minUppercase: 1, 
+            minNumbers: 1,
+            minSymbols: 1,
+            returnScore: true,
+            pointsPerUnique: 1,
+            pointsPerRepeat: 0.5,
+            pointsForContainingLower: 10,
+            pointsForContainingUpper: 10,
+            pointsForContainingNumber: 10,
+            pointsForContainingSymbol: 10 }) < 40){
+    
+            
+            alert('Password not strong');
+    
+            return;
+    
+        }
+    
+        if(FORMDATA.get('password') !== FORMDATA.get('passwordConfirm')){
+    
+            
+            alert('Password not matching');
+    
+            return;
+            
+        }
+    
+        let JSONRequest: any = {};
+        FORMDATA.forEach((value, key) => {
+    
+            if(key === 'email'){
+                value = Validator.normalizeEmail(value);
+            }
+    
+            JSONRequest[key] = Validator.escape(value);
+        
+        });
+    
+        //POST request to auth
+        await fetch(URL, {
+    
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(JSONRequest),
+    
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => {
+            
+            alert(`Error ${error.code}\n${error.message}`)
+            console.log(error)
+        
+        })
+    
+    }
+    
+    const ToggleLoginVisible: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    
+        const login_panel: HTMLFormElement = document.querySelector('form#login-panel')!;
+        const register_panel: HTMLFormElement = document.querySelector('form#register-panel')!;
+    
+        login_panel?.classList.remove('flex');
+        login_panel?.classList.add('hidden');
+    
+        register_panel?.classList.remove('hidden');
+        register_panel?.classList.add('flex');
+    
+    }
+    
+    const ToggleRegisterVisible: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    
+        const login_panel: HTMLFormElement = document.querySelector('form#login-panel')!;
+        const register_panel: HTMLFormElement = document.querySelector('form#register-panel')!;
+    
+        register_panel?.classList.remove('flex');
+        register_panel?.classList.add('hidden');
+    
+        login_panel?.classList.remove('hidden');
+        login_panel?.classList.add('flex');
+        
+    }
+
     return (
         <div className="py-10 pt-5 px-10 mb-5 bg-teal-700 border-4 border-solid border-white">
             
@@ -24,8 +171,8 @@ export default function LoginForm(){
                     <p className='mx-auto text-center text-xl font-bold select-none text-white'>User Log in</p>
                 </div>
 
-                <input name="login_email" type="email" placeholder='Email' title='Enter Email' className="px-3 py-2 my-2 text-cyan-950 font-medium" required></input>
-                <input name="login_password" type="password" placeholder='Password' title='Enter Password' className="px-3 py-2 my-2 text-cyan-950 font-medium" required></input>
+                <input name="identity" type="email" placeholder='Email' title='Enter Email' className="px-3 py-2 my-2 text-cyan-950 font-medium" required></input>
+                <input name="password" type="password" placeholder='Password' title='Enter Password' className="px-3 py-2 my-2 text-cyan-950 font-medium" required></input>
                 <button id="login_button" type="submit" className="my-2 py-2 bg-cyan-950 border-3 border-solid border-white">Log in</button>
                 <button type="button" className='mt-5' 
                     onClick={ToggleLoginVisible}
@@ -58,117 +205,4 @@ export default function LoginForm(){
             </form>
         </div>
     )
-}
-
-function loginUser(event: React.FormEvent<HTMLFormElement>): void{
-    event.preventDefault();
-    
-    const formData: FormData = new FormData(event.currentTarget);
-
-}
-
-async function registerUser(event: React.FormEvent<HTMLFormElement>): Promise<void>{
-    event.preventDefault();
-
-    const URL: RequestInfo = "http://127.0.0.1:8090/api/collections/users/records";
-    const FORMDATA: FormData = new FormData(event.currentTarget);
-    const headers: Headers = new Headers();
-
-    headers.append('Content-Type', 'application/json');
-
-
-    if(!Validator.isEmail(FORMDATA.get('email'))){
-
-        
-        alert('Invalid Email');
-
-        return;
-
-    }
-
-    if(Validator.isStrongPassword(FORMDATA.get('password'), 
-    { 
-        minLength: 8, 
-        minLowercase: 1, 
-        minUppercase: 1, 
-        minNumbers: 1,
-        minSymbols: 1,
-        returnScore: true,
-        pointsPerUnique: 1,
-        pointsPerRepeat: 0.5,
-        pointsForContainingLower: 10,
-        pointsForContainingUpper: 10,
-        pointsForContainingNumber: 10,
-        pointsForContainingSymbol: 10 }) < 40){
-
-        
-        alert('Password not strong');
-
-        return;
-
-    }
-
-    if(FORMDATA.get('password') !== FORMDATA.get('passwordConfirm')){
-
-        
-        alert('Password not matching');
-
-        return;
-        
-    }
-
-    let JSONRequest: any = {};
-    FORMDATA.forEach((value, key) => {
-
-        if(key === 'email'){
-            value = Validator.normalizeEmail(value);
-        }
-
-        JSONRequest[key] = Validator.escape(value);
-    
-    });
-
-    //POST request to auth
-    await fetch(URL, {
-
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(JSONRequest),
-
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => {
-        
-        alert(`Error ${error.code}\n${error.message}`)
-        console.log(error)
-    
-    })
-
-}
-
-const ToggleLoginVisible: React.MouseEventHandler<HTMLButtonElement> = (event) => {
-
-    const login_panel: HTMLFormElement = document.querySelector('form#login-panel')!;
-    const register_panel: HTMLFormElement = document.querySelector('form#register-panel')!;
-
-    login_panel?.classList.remove('flex');
-    login_panel?.classList.add('hidden');
-
-    register_panel?.classList.remove('hidden');
-    register_panel?.classList.add('flex');
-
-}
-
-const ToggleRegisterVisible: React.MouseEventHandler<HTMLButtonElement> = (event) => {
-
-    const login_panel: HTMLFormElement = document.querySelector('form#login-panel')!;
-    const register_panel: HTMLFormElement = document.querySelector('form#register-panel')!;
-
-    register_panel?.classList.remove('flex');
-    register_panel?.classList.add('hidden');
-
-    login_panel?.classList.remove('hidden');
-    login_panel?.classList.add('flex');
-    
 }
--- webrtc-chat/src/middleware.ts
+++ webrtc-chat/src/middleware.ts
@@ -6,15 +6,12 @@ export async function middleware(request: NextRequest) {
 
   const res = NextResponse.next();
   const pathname = request.nextUrl.pathname;
+  const token: String = request.cookies.get('njsa')?.value || "";
 
   if (requireAuth.some((path) => pathname.startsWith(path))) {
-    const token = false;/*await getToken({
-      req: request,
-      secret: process.env.SECRET,
-    });*/
 
     //check not logged in
-    if (!token) {
+    if (AuthorizeToken(token)) {
       const url = new URL('/login', request.url);
       return NextResponse.redirect(url);
     }else{
@@ -24,4 +21,31 @@ export async function middleware(request: NextRequest) {
 
   return res;
 
+}
+
+//TODO: Fix this to authenticate with user from token cookie!!!!
+
+const AuthorizeToken = async (token: String): boolean => {
+
+  try{
+    const URL = 'http://127.0.0.1:8090/api/collections/users/records'
+    const headers: Headers = new Headers();
+    
+    headers.set('Content-Type', 'application/json')
+
+    headers.append('Authorization', `Bearer ${token}`)
+
+    const authResponse = await fetch(URL, {method: 'GET', headers: headers});
+    const textDecoder = new TextDecoderStream();
+
+    if(!authResponse.ok){
+      throw new Error('Network response was not ok');
+    }
+
+  }catch(error){
+
+    console.log(error);
+    return false;
+
+  }
 }
